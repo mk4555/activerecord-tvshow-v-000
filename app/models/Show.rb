20fe8f2b32840e49f8015ff1af57fb3cfd0bf8a8
Min Kim <mk4555@nyu.edu>
2018-01-11 16:45:16 +0000

    automatically backed up by learn

--- app/models/Show.rb
+++ app/models/Show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.minimum(:rating)
+    Show.where('rating = ?', self.lowest_rating).first
   end
 
   def self.ratings_sum
